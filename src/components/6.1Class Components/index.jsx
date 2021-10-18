9809fcd6d52c551eb3763bbf50763f125eb856f1
wajdehbaish <wajdehbaish@gmail.com>
2021-10-18 13:55:44 +0300

    6.1

---
+++ src/components/6.1Class Components/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Box2 from "./box2";
+class Box1 extends React.Component {
+  render(){
+      return (
+          <div className='Box1'> <Box2/></div>
+      )
+  }
+  
+}
+
+export default Box1;
